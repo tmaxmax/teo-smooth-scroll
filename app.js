e2fbd173c4c1d2a9a6ad448e448656c25932bfbe
Teodor Maxim <tmaxmax@outlook.com>
2020-04-18 18:27:33 +0300

    Deleted test code

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
 'use strict';
-
-/**
- * Logs "sarmale" to console, along with a chosen number.
- * @param {number} elem random number at choice
- */
-function sarmale(elem) {
-  console.log(`sarmale ${elem}`);
-}
-
-sarmale(3);
