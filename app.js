9bce42d10e07be4b31322b906383620f3e037fe6
Teodor Maxim <tmaxmax@outlook.com>
2020-04-18 18:26:29 +0300

    Test for ESLint

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Logs "sarmale" to console, along with a chosen number.
+ * @param {number} elem random number at choice
+ */
+function sarmale(elem) {
+  console.log(`sarmale ${elem}`);
+}
+
+sarmale(3);
